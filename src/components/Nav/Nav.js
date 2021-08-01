07aced2e2a16ac86c958a46f402c9692584d2436
Keith Powell <keith@tapcart.co>
2021-08-01 13:54:19 -0700

    Added Nav css

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,14 +2,19 @@
 // 1. Create the function of the Component
 // 2. Export the Component 
 
+import "./Nav.css"
 
 function Nav() {
     return (
-        <header>
-            <nav>
-                <span className="icon">Twitch</span>
+        <header className="header">
+            <nav className="header__left">
+                <span className="icon"><img src="https://static.twitchcdn.net/assets/favicon-32-e29e246c157142c94346.png" alt="logo" /></span>
+                <a href="/" className="nav__item">Following</a>
+                <a href="/" className="nav__item">Browse</a>
+                <a href="/" className="nav__item">Esports</a>
+                <a href="/" className="nav__item">Music</a>
             </nav>
-            <nav>
+            <nav className="header__right">
                 <span className="user">Keith</span>
             </nav>
         </header>
